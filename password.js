abe15bb8ec8d9c4c94b982befa53ae0846e9050f
Eric O'Brien <obrienjeric@gmail.com>
2021-09-10 17:50:21 -0400

    password validator is working

---
+++ password.js
@@ -0,0 +1,13 @@
+function passwordValidator(name) {
+    console.log(`Hello, ${name}!  Welcome to the password validator!`);
+    const password = prompt("please enter your password to begin the evaluation", "Password:");
+        if(password.length > 10) {
+            console.log('NICE!  Your password is acceptable.');
+        } else {
+            prompt('FAILURE: please create a password greater than 10 characters.', "Password");
+              
+        }
+
+}
+
+passwordValidator('Eric O');
