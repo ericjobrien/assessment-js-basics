8fb49d6aa45ec465264cc056b5349850b6d39c72
Eric O'Brien <obrienjeric@gmail.com>
2021-09-10 17:53:01 -0400

    minor update to if statement operator

--- password.js
+++ password.js
@@ -1,7 +1,7 @@
 function passwordValidator(name) {
     console.log(`Hello, ${name}!  Welcome to the password validator!`);
     const password = prompt("please enter your password to begin the evaluation", "Password:");
-        if(password.length > 10) {
+        if(password.length >= 10) {
             console.log('NICE!  Your password is acceptable.');
         } else {
             prompt('FAILURE: please create a password greater than 10 characters.', "Password");
