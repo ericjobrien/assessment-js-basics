d2ed71a52538566b40b7eef9cdab7a0685e82e18
Eric O'Brien <obrienjeric@gmail.com>
2021-09-12 13:44:59 -0400

    added special character condition to password

--- password.js
+++ password.js
@@ -1,10 +1,33 @@
 function passwordValidator(name) {
+    
     console.log(`Hello, ${name}!  Welcome to the password validator!`);
+    
     const password = prompt("please enter your password to begin the evaluation", "Password:");
-        if(password.length >= 10) {
-            console.log('NICE!  Your password is acceptable.');
+    //creating special characters list array to loop through in order to check for the special characters conditon of a password
+    const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
+
+    let charArr = [];
+
+function passwordThing() {
+for(let i = 0; i < specialCharacters.length; i++) {
+  for(let j = 0; j < password.length; j++) {
+      if(specialCharacters[i] === password[j]) {
+        charArr.push(specialCharacters[i]);
+      }
+    }
+  }
+
+  if(charArr.length > 0) {
+      return charArr;
+  }
+ }
+
+    passwordThing();
+    
+        if(password.length >= 10 && charArr.length > 0) {
+            return 'NICE!  Your password is acceptable.';
         } else {
-            prompt('FAILURE: please create a password greater than 10 characters.', "Password");
+            prompt('FAILURE: Your password must be 10 characters long and must contain a special character.', "Password");
               
         }
 
